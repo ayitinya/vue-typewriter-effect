111712736d6a4df396d7c2e100ba2be27c07ae45
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2023-01-03 10:32:14 +0000

    feat: adds ga

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,7 +6,9 @@ export default defineConfig({
     lastUpdated: true,
     base: '/vue-typewriter-effect/',
     head: [
-        ['meta', { name: 'google-site-verification', content: 'onpD-kiujyUqtBDe3H5b3XUsxmo6sEriHcamATCYOvI' }]
+        ['meta', { name: 'google-site-verification', content: 'onpD-kiujyUqtBDe3H5b3XUsxmo6sEriHcamATCYOvI' }],
+        ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-PJ6JK3J6WD' }],
+        ['script', {}, `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);};gtag('js', new Date());gtag('config', 'G-PJ6JK3J6WD');`],
     ],
     themeConfig: {
         sidebar: [
